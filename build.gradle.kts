2b3c7490e1adec0b80a66bf779aaac58a8c8c290
danielnavarrowo <dani.navarro281@gmail.com>
2026-02-17 17:19:04 -0600

    Refactor: Update Gradle dependencies and Kotlin version for improved compatibility and performance

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.compose.compiler) apply false
-    kotlin("plugin.serialization") version "2.3.0"
+    kotlin("plugin.serialization") version "2.3.10"
 }
 repositories {
     google()
